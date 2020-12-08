d7be9e43ae19901ec27d94f51162aeb59d300043
vrudkovsky <vpcomtek@gmail.com>
2020-12-08 03:12:40 +0200

    Hello Tamriel

---
+++ sprint-00/t00/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/victor/Documents/ucode/half-marathon-cpp/sprint-00/t00
+# Build directory: /home/victor/Documents/ucode/half-marathon-cpp/sprint-00/t00/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
