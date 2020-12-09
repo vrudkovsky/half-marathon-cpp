e514eddb2e05effd946229bde88316ad3e1ca425
vrudkovsky <vpcomtek@gmail.com>
2020-12-09 23:35:10 +0200

    -- walletManager

---
+++ sprint-00/t02/walletManager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+struct Wallet {
+    int septims;
+};
+
+Wallet* createWallet(int value);
+void destroyWallet(Wallet* wallet);
+Wallet* createWallets(Wallet* wallets);
+void destroyWallets(Wallet* wallets);
