958227aa58fb5087f542f4a3d3e1e93c76b0ad69
vrudkovsky <vpcomtek@gmail.com>
2020-12-10 22:40:14 +0200

    -- wellet manager

--- sprint-00/t02/main.cpp
+++ sprint-00/t02/main.cpp
@@ -7,10 +7,7 @@ int main() {
 
     int amount = 5;
     Wallet* wallets = createWallets(amount);
-    for (int i = 0; i < amount; ++i) {
-        wallets[i].septims = i * i;
-        std::cout << i << " wallet: " << wallets[i].septims << " septims." << std::endl;
-    }
+
     destroyWallets(wallets);
 
     return 0;
--- sprint-00/t02/walletManager.cpp
+++ sprint-00/t02/walletManager.cpp
@@ -13,6 +13,10 @@ void destroyWallet(Wallet* wallet) {
 
 Wallet* createWallets(int amount) {
     Wallet* wallets = new Wallet[amount];
+    for (int i = 0; i < amount; ++i) {
+        wallets[i].septims = i * i;
+        std::cout << i << " wallet: " << wallets[i].septims << " septims." << std::endl;
+    }
     return wallets;
 };
 void destroyWallets(Wallet* wallets) {
